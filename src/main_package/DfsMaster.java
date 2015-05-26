470e9d59c9c9b7605491d0bb4470be17838638ec
houssainy <mohamed.houssainy@gmail.com>
2015-05-26 21:10:50 +0200

    MasterToPrimaryInterface changed to PrimaryToMasterInterface

--- src/main_package/DfsMaster.java
+++ src/main_package/DfsMaster.java
@@ -9,9 +9,10 @@ import java.util.HashMap;
 import java.util.Random;
 
 import rmi_interface_package.MasterServerInterface;
+import utilities_package.Log;
 
-
-public class DfsMaster implements MasterServerInterface, MasterToPrimaryInterface {
+public class DfsMaster implements MasterServerInterface,
+		PrimaryToMasterInterface {
 
 	private BufferedReader metaData;
 	private HashMap<String, String> tempFiles;
@@ -20,37 +21,37 @@ public class DfsMaster implements MasterServerInterface, MasterToPrimaryInterfac
 	private String[] ips;
 	private long id = 0;
 	private Log log;
-	
+
 	public DfsMaster() throws IOException {
 		metaData = new BufferedReader(new FileReader("config/MetaData"));
 		tempFiles = new HashMap<>();
 		metaDataHash = new HashMap<>();
 		transactions = new HashMap<>();
 		log = Log.createInstance();
-		
+
 		String line;
 		while ((line = metaData.readLine()) != null) {
 			String[] splittedLine = line.split(":");
 			metaDataHash.put(splittedLine[0], splittedLine[1]);
 		}
-		
-		BufferedReader br = new BufferedReader(new FileReader("config/replicaServers"));
+
+		BufferedReader br = new BufferedReader(new FileReader(
+				"config/replicaServers"));
 		int noOfIps = Integer.parseInt(br.readLine());
 		for (int i = 0; i < noOfIps; i++)
 			ips[i] = br.readLine();
 		br.close();
-		
+
 		log.write("Master Machine Started and Working.");
 	}
-	
+
 	@Override
 	public String read(String fileName) throws FileNotFoundException,
 			IOException, RemoteException {
 		if (metaDataHash.containsKey(fileName)) {
 			log.write("Reading file " + fileName);
 			return metaDataHash.get(fileName);
-		}
-		else {
+		} else {
 			log.write("ERROR: File " + fileName + " Not found.");
 			throw new FileNotFoundException();
 		}
@@ -60,15 +61,16 @@ public class DfsMaster implements MasterServerInterface, MasterToPrimaryInterfac
 	public String newTxn(String fileName) throws RemoteException, IOException {
 		if (metaDataHash.containsKey(fileName)) {
 			transactions.put(id, fileName);
-			log.write("Created new Transaction with id: " + id + "for file: " + fileName);
+			log.write("Created new Transaction with id: " + id + "for file: "
+					+ fileName);
 			return (id++) + "," + metaDataHash.get(fileName);
-		}
-		else {
+		} else {
 			Random rand = new Random();
 			String primReplica = ips[rand.nextInt(ips.length - 1)];
 			tempFiles.put(fileName, primReplica);
 			transactions.put(id, fileName);
-			log.write("File not found. Creating new file. Created new Transaction with id: " + id + "for file: " + fileName);
+			log.write("File not found. Creating new file. Created new Transaction with id: "
+					+ id + "for file: " + fileName);
 			return (id++) + "," + tempFiles.get(fileName);
 		}
 	}
@@ -77,12 +79,11 @@ public class DfsMaster implements MasterServerInterface, MasterToPrimaryInterfac
 	public void abortTransaction(long txnID) {
 		String fileName = transactions.get(txnID);
 		transactions.remove(txnID);
-		
+
 		if (tempFiles.containsKey(fileName)) {
 			tempFiles.remove(fileName);
 			log.write("Transaction aborted. Temp file " + fileName);
-		}
-		else
+		} else
 			log.write("Transaction aborted.");
 	}
 
@@ -90,7 +91,7 @@ public class DfsMaster implements MasterServerInterface, MasterToPrimaryInterfac
 	public void commitTansaction(long txnID) {
 		String fileName = transactions.get(txnID);
 		transactions.remove(txnID);
-		
+
 		if (tempFiles.containsKey(fileName)) {
 			metaDataHash.put(fileName, tempFiles.get(fileName));
 			tempFiles.remove(fileName);
--- src/main_package/PrimaryToMasterInterface.java
+++ src/main_package/PrimaryToMasterInterface.java
@@ -3,7 +3,7 @@ package main_package;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface MasterToPrimaryInterface extends Remote {
+public interface PrimaryToMasterInterface extends Remote {
 	public void commitTansaction(long txnID) throws RemoteException;
 	public void abortTransaction(long txnID) throws RemoteException;
 }
