28c11cee10df152b8bb91a4d2e663200d2db5c3d
houssainy <mohamed.houssainy@gmail.com>
2015-05-27 00:54:58 +0200

    ss

--- src/main_package/Constants.java
+++ src/main_package/Constants.java
@@ -1,6 +1,6 @@
 package main_package;
 
 public class Constants {
-	public final static int RMI_REGISTRY_PORT = 9642;
+	public final static int RMI_REGISTRY_PORT = 8080;
 	public final static String RMI_NAME = "DFS_RMI";
 }
