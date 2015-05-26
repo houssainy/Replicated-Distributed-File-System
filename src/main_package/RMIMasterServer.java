c3758c92a99ecf43398ff1906c46fd9a13f32f34
houssainy <mohamed.houssainy@gmail.com>
2015-05-27 00:52:49 +0200

    RMI config fixed

--- src/main_package/RMIMasterServer.java
+++ src/main_package/RMIMasterServer.java
@@ -1,16 +1,23 @@
 package main_package;
 
+import java.io.File;
 import java.io.IOException;
 import java.rmi.registry.LocateRegistry;
+import java.util.Scanner;
 
 public class RMIMasterServer {
 	public static void main(String[] args) throws IOException {
-		System.setProperty("java.rmi.server.hostname", "localhost");
+		Scanner fileReader = new Scanner(new File("conf/master_ip"));
+		String masterIp = fileReader.next();
+		fileReader.close();
+
+		System.out.println("Master Ip = " + masterIp);
+		System.setProperty("java.rmi.server.hostname", masterIp);
 
 		DfsMaster masterServr = new DfsMaster();
 
-		LocateRegistry.createRegistry(Constants.RMI_REGISTRY_PORT).rebind(Constants.RMI_NAME,
-				masterServr);
+		LocateRegistry.createRegistry(Constants.RMI_REGISTRY_PORT).rebind(
+				Constants.RMI_NAME, masterServr);
 
 		System.out.println("DfsMaster Registred to Registry Server...");
 	}
