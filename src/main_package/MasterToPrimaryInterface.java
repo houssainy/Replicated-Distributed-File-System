1faa987e3cb90466c94bf1c4491948bff9daf8c9
houssainy <mohamed.houssainy@gmail.com>
2015-05-27 22:24:43 +0200

    logs added

--- src/main_package/MasterToPrimaryInterface.java
+++ src/main_package/MasterToPrimaryInterface.java
@@ -1,9 +1,8 @@
 package main_package;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
-public interface MasterToPrimaryInterface extends Remote{
-	
-	public void newTransaction(long txnID, String fileName);
-	
+public interface MasterToPrimaryInterface extends Remote {
+	public void newTransaction(long txnID, String fileName) throws RemoteException;
 }
